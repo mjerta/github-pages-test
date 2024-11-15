8db6caa6545ee6156aa0a7c49acbbc69e47f1865
mjerta <maarten.postma1000@gmail.com>
2024-11-15 23:56:29 +0100

    Add routing to test

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,15 @@
-import {useState} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {Route, Router, Routes} from "react-router-dom";
+import Profile from "./pages/profile/Profile.jsx";
+import Home from "./pages/home/Home.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>Test - Github Pages</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad
-         architecto, aspernatur at dicta, doloribus iusto non possimus provident
-         qui quo recusandae repellat rerum saepe, sit totam veniam vero. Cumque
-         deserunt esse et, incidunt iusto laborum magnam modi numquam pariatur
-         porro repellat soluta ut vero? Ab accusantium adipisci alias amet at
-         atque blanditiis commodi consequuntur dolorem doloribus ducimus error
-         et eveniet excepturi illo in ipsum, laudantium magni maxime, modi
-         molestiae neque non obcaecati perferendis perspiciatis porro qui quidem
-         quod repudiandae similique sit ut vero vitae. Accusamus distinctio
-         dolorum eaque id ipsum magnam necessitatibus, nemo nesciunt nostrum
-         officia quo reiciendis temporibus. Dolorem, illum, reiciendis. Aperiam
-         deleniti nemo quidem quis sed suscipit voluptatibus. Ab architecto
-         distinctio eveniet illum, impedit nemo odit quae? Deserunt, distinctio
-         dolore eligendi fugit, id inventore maiores, nobis officia pariatur
-         perspiciatis tempora vel veritatis! Accusamus dolorum eligendi error
-         incidunt tenetur voluptate. Animi eaque illo minus, quidem rem sit
-         voluptatibus. Alias!</p>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+      </Routes>
     </>
   )
 }
