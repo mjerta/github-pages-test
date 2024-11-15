546e47ccf22658fbc6cff4dcd769c054d98558fc
Maarten Postma <maarten.postma1000@gmail.com>
2024-11-15 17:21:27 +0100

    Setup config for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: true, // Automatically opens the browser on server start
+    watch: {
+      usePolling: true, // Ensures changes are detected, useful in some environments
+    },
+  },
+  base: "/github-pages-test"
 })
