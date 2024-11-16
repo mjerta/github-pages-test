78b81bc3f8b6edcb5f4018f56abe8728b3ac434f
Maarten Postma <maarten.postma1000@gmail.com>
2024-11-16 01:20:24 +0100

    Add env file and variable for testing

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,12 @@
-function Home () {
+function Home() {
+  const test = import.meta.env.VITE_TEST_VARIABLE;
+
   return (
-    <h1>Home  Page</h1>
+    <>
+      <h1>Home Page</h1>
+      <h3>{test}</h3>
+    </>
   )
 }
+
 export default Home;
